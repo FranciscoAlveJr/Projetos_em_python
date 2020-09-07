e767484b6b1f88cbc1f2bb8e135de0cf881d8aa8
FranciscoAlveJr <65497402+FranciscoAlveJr@users.noreply.github.com>
2020-09-07 17:09:49 -0300

    Create Função-área.py
    Função que permite caluclar a área de um terreno

---
+++ Função-área.py
@@ -0,0 +1,9 @@
+def area(com, lar):
+    a = com * lar
+    print(f'A área do terreno {com}x{lar} é de {a}m²')
+
+
+c = float(input('Comprimento (metros): '))
+l = float(input('Largura (metros): '))
+area(c, l)
+
